5e7b30cff63a30930c1d2051f977369f8a64ad2c
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-24 08:26:04 +0900

    ✨ :: 회원탈퇴 기능 구현

--- src/main/java/com/example/cameit/domain/user/presentation/UserController.java
+++ src/main/java/com/example/cameit/domain/user/presentation/UserController.java
@@ -7,7 +7,9 @@ import com.example.cameit.domain.user.presentation.dto.request.UserUpdatePwReque
 import com.example.cameit.domain.user.service.UserSigninService;
 import com.example.cameit.domain.user.service.UserSignupService;
 import com.example.cameit.domain.user.service.UserUpdatePwService;
+import com.example.cameit.domain.user.service.WithdrawlService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +20,26 @@ public class UserController {
     private final UserSignupService userSignupService;
     private final UserSigninService userSigninService;
     private final UserUpdatePwService userUpdatePwService;
+    private final WithdrawlService withdrawlService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public void signUp(@RequestBody @Validated UserSignupRequest request) {
         userSignupService.execute(request);
     }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/login")
     public TokenResponse signIn(@RequestBody UserSigninRequest request) {
         return userSigninService.execute(request);
     }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/password")
     public void updatePw(@RequestBody UserUpdatePwRequest request) {
         userUpdatePwService.execute(request);
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/")
+    public void withdrawl() {
+        withdrawlService.execute();
     }
 }
