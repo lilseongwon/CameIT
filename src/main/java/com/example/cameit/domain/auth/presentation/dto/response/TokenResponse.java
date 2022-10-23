23285136bdfce44de68ed7bd217eb9f8b9e34eda
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-23 20:32:36 +0900

    📝 :: TokenResponse

---
+++ src/main/java/com/example/cameit/domain/auth/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.example.cameit.domain.auth.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TokenResponse {
+
+    private final String accessToken;
+    private final String refreshToken;
+}
