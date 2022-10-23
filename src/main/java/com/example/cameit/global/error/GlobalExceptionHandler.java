da84788872d158e6e8163221827a8b4d4e02a746
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-23 20:09:19 +0900

    📝 :: GlobalExceptionHandler

--- src/main/java/com/example/cameit/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/cameit/global/error/GlobalExceptionHandler.java
@@ -1,5 +1,15 @@
 package com.example.cameit.global.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
