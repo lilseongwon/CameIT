becda4ec5dc772bf1ff5c25a19a75fa789819903
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-24 08:00:41 +0900

    ♻️ :: 토큰 발급절차 변경

--- src/main/java/com/example/cameit/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/cameit/global/security/jwt/JwtTokenProvider.java
@@ -2,8 +2,6 @@ package com.example.cameit.global.security.jwt;
 
 import com.example.cameit.domain.auth.domain.RefreshToken;
 import com.example.cameit.domain.auth.domain.repository.RefreshTokenRepository;
-import com.example.cameit.domain.auth.presentation.dto.response.TokenResponse;
-import com.example.cameit.global.security.auth.AuthDetailsService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
@@ -20,28 +18,27 @@ public class JwtTokenProvider {
 
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtProperties jwtProperties;
-    private final AuthDetailsService authDetailsService;
 
-    public TokenResponse generateTokens(String accountId) {
-        String accessToken = generateToken(accountId, ACCESS_KEY, jwtProperties.getAccessExp());
-        String refreshToken = generateToken(accountId, REFRESH_KEY, jwtProperties.getRefreshExp());
-
-        refreshTokenRepository.save(RefreshToken.builder()
-                .accountId(accountId)
-                .token(refreshToken)
-                .ttl(jwtProperties.getRefreshExp())
-                .build());
-
-        return TokenResponse.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
+    public String generateAccessToken(String email) {
+        return generateToken(email, ACCESS_KEY, jwtProperties.getAccessExp());
     }
 
-    private String generateToken(String accountId, String type, Long exp) {
+    public String generateRefreshToken(String email) {
+        String refreshToken =  generateToken(email, REFRESH_KEY, jwtProperties.getRefreshExp());
+
+        refreshTokenRepository.save(RefreshToken.builder()
+                .email(email)
+                .token(refreshToken)
+                .ttl(jwtProperties.getRefreshExp() * 1000)
+                .build());
+
+        return refreshToken;
+    }
+
+    private String generateToken(String email, String type, Long exp) {
         return Jwts.builder()
                 .signWith(SignatureAlgorithm.ES256, jwtProperties.getSecret())
-                .setSubject(accountId)
+                .setSubject(email)
                 .setHeaderParam("typ", type)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + exp * 1000))
