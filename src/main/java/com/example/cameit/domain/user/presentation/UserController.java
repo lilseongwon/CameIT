76125df3d536ae94b7abf3d254683504510308de
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-24 08:16:49 +0900

    ✨ :: 로그인 로직 구현

--- src/main/java/com/example/cameit/domain/user/presentation/UserController.java
+++ src/main/java/com/example/cameit/domain/user/presentation/UserController.java
@@ -1,6 +1,9 @@
 package com.example.cameit.domain.user.presentation;
 
+import com.example.cameit.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.cameit.domain.user.presentation.dto.request.UserSigninRequest;
 import com.example.cameit.domain.user.presentation.dto.request.UserSignupRequest;
+import com.example.cameit.domain.user.service.UserSigninService;
 import com.example.cameit.domain.user.service.UserSignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -14,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
     private final UserSignupService userSignupService;
+    private final UserSigninService userSigninService;
 
     @PostMapping("/signup")
     public void signUp(@RequestBody @Validated UserSignupRequest request) {
         userSignupService.execute(request);
+    }
+    @PostMapping("/login")
+    public TokenResponse signIn(@RequestBody UserSigninRequest request) {
+        return userSigninService.execute(request);
     }
 }
