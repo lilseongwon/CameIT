e05d4d13e92762345b957acb27a150896a4c997a
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-23 18:24:15 +0900

    ✨ :: 프로젝트 생성

---
+++ src/main/java/com/example/cameit/CameItApplication.java
@@ -0,0 +1,13 @@
+package com.example.cameit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CameItApplication.class, args);
+    }
+
+}
