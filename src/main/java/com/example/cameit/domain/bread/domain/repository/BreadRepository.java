c81fe77b0852ff52e44cdea7a5ca937a5b1f3d7f
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-24 09:28:56 +0900

    📝 :: BreadRepository

---
+++ src/main/java/com/example/cameit/domain/bread/domain/repository/BreadRepository.java
@@ -0,0 +1,7 @@
+package com.example.cameit.domain.bread.domain.repository;
+
+import com.example.cameit.domain.bread.domain.Bread;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BreadRepository extends CrudRepository<Bread, Long> {
+}
