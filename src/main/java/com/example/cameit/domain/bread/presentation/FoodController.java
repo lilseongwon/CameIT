34fe2f6fbcff432e245739af95cce870778135e4
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-24 12:30:41 +0900

    ✨ :: 빵정보 업데이트 기능 구현

--- src/main/java/com/example/cameit/domain/bread/presentation/FoodController.java
+++ src/main/java/com/example/cameit/domain/bread/presentation/FoodController.java
@@ -2,22 +2,27 @@ package com.example.cameit.domain.bread.presentation;
 
 import com.example.cameit.domain.bread.presentation.dto.request.BreadRequest;
 import com.example.cameit.domain.bread.service.AddBreadService;
+import com.example.cameit.domain.bread.service.UpdateBreadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class FoodController {
     private final AddBreadService addFoodService;
+    private final UpdateBreadService updateBreadService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/")
     public void addFood(@RequestBody @Validated BreadRequest request) {
         addFoodService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{bread-id}")
+    public void execute(@PathVariable("bread-id") Long id, @RequestBody @Validated BreadRequest Request) {
+        updateBreadService.execute(id, Request);
     }
 }
