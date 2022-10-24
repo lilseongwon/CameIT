952655808d6f4f4265d600709b9ef15cf2d84bb9
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-24 11:46:45 +0900

    ⚡ :: findByName 추가

--- src/main/java/com/example/cameit/domain/bread/domain/repository/BreadRepository.java
+++ src/main/java/com/example/cameit/domain/bread/domain/repository/BreadRepository.java
@@ -3,5 +3,8 @@ package com.example.cameit.domain.bread.domain.repository;
 import com.example.cameit.domain.bread.domain.Bread;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BreadRepository extends CrudRepository<Bread, Long> {
+    Optional<Bread> findByName(String name);
 }
