7f1e53f5660f3278a3cfbc4b24e429b9b2495b9c
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-24 12:17:03 +0900

    📝 :: AddBreadService

---
+++ src/main/java/com/example/cameit/domain/bread/service/AddBreadService.java
@@ -0,0 +1,37 @@
+package com.example.cameit.domain.bread.service;
+
+import com.example.cameit.domain.bread.domain.Bread;
+import com.example.cameit.domain.bread.domain.repository.BreadRepository;
+import com.example.cameit.domain.bread.exception.BreadExistException;
+import com.example.cameit.domain.bread.presentation.dto.request.BreadRequest;
+import com.example.cameit.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AddBreadService {
+    private final BreadRepository breadRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute(BreadRequest request) {
+        userFacade.checkPermission();
+        checkFoodExist(request.getName());
+
+        breadRepository.save(
+                Bread.builder()
+                        .name(request.getName())
+                        .category(request.getCategory())
+                        .explain(request.getExplain())
+                        .imageUrl(request.getImage_url())
+                        .price(request.getPrice())
+                        .build());
+    }
+
+    private void checkFoodExist(String name) {
+            if(breadRepository.findByName(name).isPresent())
+                throw BreadExistException.EXCEPTION;
+    }
+}
