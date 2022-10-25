edc97a7fb9ddaa53c6f2290d7914b3c940b76db1
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-25 10:59:48 +0900

    ✨ :: 빵 검색기능 구현

--- src/main/java/com/example/cameit/domain/bread/presentation/BreadController.java
+++ src/main/java/com/example/cameit/domain/bread/presentation/BreadController.java
@@ -1,8 +1,10 @@
 package com.example.cameit.domain.bread.presentation;
 
 import com.example.cameit.domain.bread.presentation.dto.request.BreadRequest;
+import com.example.cameit.domain.bread.presentation.dto.response.QueryBreadListResponse;
 import com.example.cameit.domain.bread.service.AddBreadService;
 import com.example.cameit.domain.bread.service.DeleteBreadService;
+import com.example.cameit.domain.bread.service.QueryBreadsByKeywordService;
 import com.example.cameit.domain.bread.service.UpdateBreadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/bread")
 public class BreadController {
     private final AddBreadService addFoodService;
     private final UpdateBreadService updateBreadService;
     private final DeleteBreadService deleteBreadService;
+    private final QueryBreadsByKeywordService queryBreadsByKeywordService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/")
@@ -32,5 +36,10 @@ public class BreadController {
     @DeleteMapping("/{bread-id}")
     public void execute(@PathVariable("bread-id") Long id) {
         deleteBreadService.execute(id);
+    }
+
+    @GetMapping("/")
+    public QueryBreadListResponse queryFoodByKeyword(@RequestParam(value = "keyword")String keyword) {
+        return queryBreadsByKeywordService.execute(keyword);
     }
 }
