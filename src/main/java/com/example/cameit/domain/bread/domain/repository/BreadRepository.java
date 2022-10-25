e66c445f3e77d9ad9d95a8add748eccc61060253
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-25 11:00:15 +0900

    ⚡ :: 전체조회 메소드 추가

--- src/main/java/com/example/cameit/domain/bread/domain/repository/BreadRepository.java
+++ src/main/java/com/example/cameit/domain/bread/domain/repository/BreadRepository.java
@@ -3,8 +3,12 @@ package com.example.cameit.domain.bread.domain.repository;
 import com.example.cameit.domain.bread.domain.Bread;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BreadRepository extends CrudRepository<Bread, Long> {
     Optional<Bread> findByName(String name);
+    List<Bread> findAllByNameContains(String name);
+
+    List<Bread> findAllByOrderById();
 }
