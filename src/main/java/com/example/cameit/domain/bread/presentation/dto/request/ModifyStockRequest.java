ace2baaaa33ad81bdc8c7f17973d5f371519d18b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-26 10:28:20 +0900

    📝 :: ModifyStockRequest

---
+++ src/main/java/com/example/cameit/domain/bread/presentation/dto/request/ModifyStockRequest.java
@@ -0,0 +1,10 @@
+package com.example.cameit.domain.bread.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ModifyStockRequest {
+    private Integer modifyStock;
+}
