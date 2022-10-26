334380650f3fa40f5288bf2efbb3bcb8c6f22db7
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-26 10:12:05 +0900

    ⚡ :: 전체조회 메소드 추가

--- src/main/java/com/example/cameit/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/cameit/domain/user/domain/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.example.cameit.domain.user.domain.repository;
 import com.example.cameit.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String accountId);
+    List<User> findAllByOrderById();
 }
