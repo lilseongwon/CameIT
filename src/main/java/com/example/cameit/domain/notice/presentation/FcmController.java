802309439b137e395f6fc3d8f8257e5f704967ba
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-26 10:11:25 +0900

    ✨ :: 가게 영업상황 푸시기능 구현

---
+++ src/main/java/com/example/cameit/domain/notice/presentation/FcmController.java
@@ -0,0 +1,20 @@
+package com.example.cameit.domain.notice.presentation;
+
+import com.example.cameit.domain.notice.presentation.request.FcmStatusRequest;
+import com.example.cameit.domain.notice.service.SendOpenAndCloseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/fcm")
+public class FcmController {
+    private final SendOpenAndCloseService sendOpenAndCloseService;
+
+    public void send(@RequestBody @Validated FcmStatusRequest request) {
+        sendOpenAndCloseService.execute(request.getStatus());
+    }
+}
