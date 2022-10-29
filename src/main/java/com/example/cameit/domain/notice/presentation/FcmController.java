9c731c420644eb5f5db1a04f4fbd4b69d5265433
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-29 17:20:24 +0900

    ⚡ :: post매핑 추가

--- src/main/java/com/example/cameit/domain/notice/presentation/FcmController.java
+++ src/main/java/com/example/cameit/domain/notice/presentation/FcmController.java
@@ -4,6 +4,7 @@ import com.example.cameit.domain.notice.presentation.request.FcmStatusRequest;
 import com.example.cameit.domain.notice.service.SendOpenAndCloseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FcmController {
     private final SendOpenAndCloseService sendOpenAndCloseService;
 
+    @PostMapping("/")
     public void send(@RequestBody @Validated FcmStatusRequest request) {
         sendOpenAndCloseService.execute(request.getStatus());
     }
