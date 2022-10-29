3057dad4dd6fc450935b92d710d8084234432b02
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-29 16:40:19 +0900

    ♻️ :: NotNull로 수정

--- src/main/java/com/example/cameit/domain/bread/presentation/dto/request/BreadRequest.java
+++ src/main/java/com/example/cameit/domain/bread/presentation/dto/request/BreadRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -19,7 +20,7 @@ public class BreadRequest {
     @Size(min = 1, max= 500, message = "explain은 최대 500자 입니다.")
     private String explain;
 
-    @NotBlank(message = "price는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
+    @NotNull(message = "price는 Null을 허용하지 않습니다.")
     private Integer price;
 
     @NotBlank(message = "image_url은 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
