7d1a3f2ca441ba9f1e341d3d5fef439b04c7cc37
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-29 17:36:44 +0900

    ⚡ :: modify_stock notNull 추가

--- src/main/java/com/example/cameit/domain/bread/presentation/dto/request/ModifyStockRequest.java
+++ src/main/java/com/example/cameit/domain/bread/presentation/dto/request/ModifyStockRequest.java
@@ -3,8 +3,12 @@ package com.example.cameit.domain.bread.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class ModifyStockRequest {
+
+    @NotNull(message = "modifyStock은 Null을 허용하지 않습니다.")
     private Integer modifyStock;
 }
