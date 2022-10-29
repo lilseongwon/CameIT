a7d6ddf6581b882b29c9ba78c5b35c50ee622f4e
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-29 16:21:28 +0900

    ♻️ :: 패키지 구조 수정

--- src/main/java/com/example/cameit/global/firebase/FCMInitializer.java
+++ src/main/java/com/example/cameit/global/firebase/FCMInitializer.java
@@ -1,4 +1,4 @@
-package com.example.cameit.domain.notice.firebase;
+package com.example.cameit.global.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
